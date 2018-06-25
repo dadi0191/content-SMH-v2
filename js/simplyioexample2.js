5ef871243f82dd97584a35d71f4ec3420699b766
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-25 23:16:08 +0100

    Illegal characters
    Illegal characters

--- js/simplyioexample2.js
+++ js/simplyioexample2.js
@@ -1,1 +1,1 @@
-ajax({ url: 'http://www.goi.org.uk/index.html’ }, function(data){  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];  simply.title(headline);});
+ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){var headline = data.match(/<h1>(.*?)<\/h1>/)[1];simply.title(headline);});
