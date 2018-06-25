b4c622317b14f72acb31e55724c8c2c1b021c45a
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-26 00:35:00 +0100

    v2

--- js/simplyioexample2.js
+++ js/simplyioexample2.js
@@ -1,2 +1,2 @@
 ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
-var headline = data.match(/>Read (.*?)\(/)[1];simply.title(headline);});
+var headline = data.match(/>Read (.*?)(/)[1];simply.title(headline);});
