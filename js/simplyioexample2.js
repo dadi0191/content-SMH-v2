21219b2cce31080fdc17fd69c8b2d8c36a1072c6
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-26 22:47:13 +0100

    restructured with body text
    restructured with body text

--- js/simplyioexample2.js
+++ js/simplyioexample2.js
@@ -2,4 +2,4 @@ ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
 var headline = data.match(/currentpsalm(.*?)<\/h3>/)[1];
 var headline2 = headline.split(">");
 var dataline = data.match(/<!-- Last built (.*?) -->/)[1];
-simply.subtitle(dataline);simply.title(headline2[1]);});
+simply.text({title: headline2[1], subtitle: 'Last built', body: dataline });});
