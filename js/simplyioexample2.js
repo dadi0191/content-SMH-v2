07eded496ee1e0299be28010ad1aa38e2da49154
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-26 22:08:07 +0100

    (1) didn’t work
    (1) didn’t work

--- js/simplyioexample2.js
+++ js/simplyioexample2.js
@@ -1,3 +1,3 @@
 ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
 var headline = data.match(/currentpsalm(.*?)<\/h3>/)[1];
-var headline2 = headline.split(">");simply.title(headline2(1));});
+var headline2 = headline.split(">");simply.title(headline2);});
