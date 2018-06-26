851d7a1e5d2c1d172fe3b27d55ff640c427022f8
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-26 21:52:27 +0100

    removed innerHTML
    Removed innerHTML

--- js/simplyioexample3.js
+++ js/simplyioexample3.js
@@ -1,5 +1,5 @@
 ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
-var headline = document.getElementById("currentpsalm").innerHTML;
+var headline = document.getElementById("currentpsalm");
 simply.title(headline);
 });
 
