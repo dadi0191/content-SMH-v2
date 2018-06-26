65967d2963b46ce1738070c292f335a0f68bc71f
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-26 10:05:34 +0100

    Missed character in 1
    Missed character in 1

--- js/simplyioexample1.js
+++ js/simplyioexample1.js
@@ -1,4 +1,4 @@
-jax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
+ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
 var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
 simply.title(headline);
 });
