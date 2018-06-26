dc51c8beab20e12fcd903aa312998f6382fff396
Steve Hewitt <stevehewitt@bcs.org.uk>
2018-06-26 22:38:41 +0100

    Added last built
    Added last built

--- js/simplyioexample2.js
+++ js/simplyioexample2.js
@@ -1,3 +1,5 @@
 ajax({ url: 'http://www.goi.org.uk/index.html' }, function(data){
 var headline = data.match(/currentpsalm(.*?)<\/h3>/)[1];
-var headline2 = headline.split(">");simply.title(headline2[1]);});
+var headline2 = headline.split(">");
+var dataline = data.match(/<!-- Last built (.*?) -->/)[1];
+simply.subtitle(dataline);simply.title(headline2[1]);});
