2a3c3200aba203d2bb0ecf55d74a50d479932d88
Steve Hewitt <stevehewitt@bcs.org.uk>
2020-05-06 18:04:03 +0100

    updated to html and with sensor

--- js/testjs002.js
+++ js/testjs002.js
@@ -1,3 +1,10 @@
+<html>
+<head>
+<title>Temperatures</title>
+</head>
+<body>
+
+<script>
 ajax(
 var dynamicUrl = 'https://fcc-weather-api.glitch.me/api/current?lat=-33.87&lon=151.21';
 $.getJSON(dynamicUrl, function(data) {
@@ -6,3 +13,14 @@ $.getJSON(dynamicUrl, function(data) {
   <p> data.name()</p>
   <p> data.temp()</p>
 });
+ajax(
+var dynamicUrl = '192.168.1.116/api/Ssr-74RWMVAbFUiEUKzyyifYmpo6UEoKdT4YeFFh/sensors/13/';
+$.getJSON(dynamicUrl, function(data) {
+/* console.log("This is an example of a dynamic JSON file being served by a web server.")
+  console.log(data);*/
+  
+  <p> data.state()</p>
+});
+</script>
+</body>
+</html>
